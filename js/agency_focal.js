bd4160c49756810b8abbc29092f70688bf338dba
jsonflorendo <jsonflorendo@gmail.com>
2025-03-08 19:49:17 +0800

    update 03/08/2025 | 7:49 PM

--- js/agency_focal.js
+++ js/agency_focal.js
@@ -147,3 +147,41 @@ document.querySelectorAll('.item').forEach(item => {
         });
     });
 });
+
+
+// Adding functionality to video play and pause button
+const video=document.getElementsByClassName("custom-video_video");
+let i;
+for (i = 0; i < video.length; i++) {
+
+    video[i].addEventListener("click", function() {
+        const controls = this.nextElementSibling;
+        if (controls.innerHTML === "▶") {
+            controls.innerHTML = "| |";
+            this.play();
+        } else {
+            controls.innerHTML = "▶";
+            this.pause();
+        }
+    });
+
+    video[i].addEventListener("play", function() {
+        const controls = this.nextElementSibling;
+        if (!this.paused) {
+            controls.style.display = "none";
+        }
+    });
+
+    video[i].addEventListener("mouseout", function() {
+        const controls = this.nextElementSibling;
+        if (!this.paused) {
+            controls.style.display = "none";
+        }
+    });
+
+    video[i].addEventListener("mouseover", function() {
+        const controls = this.nextElementSibling;
+        controls.style.display = "flex";
+    });
+
+}
